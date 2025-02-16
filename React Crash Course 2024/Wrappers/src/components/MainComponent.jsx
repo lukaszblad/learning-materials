29af42d916896a96df2d548697abf82e2f9872d5
lukaszblad <lukasz.blad.97@gmail.com>
2025-02-16 20:21:56 +0100

    React Crash Course - Wrappers

---
+++ React Crash Course 2024/Wrappers/src/components/MainComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Wrapper from './Wrapper'
+
+const MainComponent = () => {
+  return (
+    <div>
+      <Wrapper>
+        <h1>wrapped content</h1>
+      </Wrapper>
+    </div>
+  )
+}
+
+export default MainComponent
---
+++ React Crash Course 2024/Wrappers/src/components/Wrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Wrapper = ({ children }) => {
+  return (
+    <div style={{color:'red'}}>
+      { children }
+    </div>
+  )
+}
+
+export default Wrapper
