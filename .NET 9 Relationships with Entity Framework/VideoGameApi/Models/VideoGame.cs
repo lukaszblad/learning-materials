ca32b55545069d9ca68cde003b87b3171fc6dd70
lukaszblad <lukasz.blad@protonmail.com>
2025-02-25 10:45:43 +0100

    .NET 9 Relationships with Entity Framework

---
+++ .NET 9 Relationships with Entity Framework/VideoGameApi/Models/VideoGame.cs
@@ -0,0 +1,15 @@
+﻿namespace VideoGameApi.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Platform { get; set; }
+        public int? DeveloperId { get; set; }
+        public Developer? Developer { get; set; }
+        public int? PublisherId { get; set; }
+        public Publisher? Publisher { get; set; }
+        public VideoGameDetails? VideoGameDetails { get; set; }
+        public List<Genre>? Genres { get; set; }
+    }
+}
---
+++ .NET 9 Relationships with Entity Framework/VideoGameApi/Models/VideoGameDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace VideoGameApi.Models
+{
+    public class VideoGameDetails
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int VideoGameId { get; set; }
+        public VideoGame VideoGame { get; set; }
+    }
+}
